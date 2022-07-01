64ba24e1db7dbfce5813209d0b78393c9899111a
Midas Lambrichts <midaslamb@gmail.com>
2022-07-01 21:57:28 +0200

    nvim: Add telescope plugin
    Allow faster navigation and live grep

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -5,3 +5,6 @@ map('n', '<leader><leader>', '<C-^>', {noremap = true, silent = true})
 map('n', '<leader>ha', ':lua require("harpoon.mark").add_file()<CR>', { noremap = true })
 map('n', '<leader>hu', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true })
 map('n', '<leader>ht1', ':lua require("harpoon.term").gotoTerminal(1)<CR>',{noremap = true})
+
+map('n', '<leader>ff', ':lua require("telescope.builtin").find_files()<CR>',{noremap = true})
+map('n', '<leader>fg', ':lua require("telescope.builtin").live_grep()<CR>',{noremap = true})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,12 +1,18 @@
 return require('packer').startup {
 	function(use)
         use 'wbthomason/packer.nvim' -- Package manager? We already have packer?
+
         use 'neovim/nvim-lspconfig'
+
         use 'williamboman/nvim-lsp-installer'
+
         use 'nvim-treesitter/nvim-treesitter'
 
         use 'nvim-lua/plenary.nvim'
         use 'ThePrimeagen/harpoon'
+
+        use { 'nvim-telescope/telescope.nvim', requires = { {'nvim-lua/plenary.nvim'} } }
+
 	end,
 	config = {
 	  package_root = vim.fn.stdpath('config') .. '/site/pack'
